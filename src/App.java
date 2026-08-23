845088b85c4292b0bae2590a375b79e639454d6e
Kauli Robinson Kleemann <kaulirk@outlook.com>
2026-08-23 19:45:36 -0300

    Adicionado: atributo ano na classe Carro. Adicionado: método compararAno na classe Carro.

--- src/App.java
+++ src/App.java
@@ -7,7 +7,18 @@ public class App {
         carro1.bancos = 4;
         carro1.radio = true;
         carro1.motor = "1.0 Flex";
+        carro1.ano = 2009;
 
         motorista1.exibirMotoristaECarro();
+
+        Carro carro2 = new Carro();
+        Motorista motorista2 = new Motorista("Andre Stela", carro2);
+
+        carro2.bancos = 4;
+        carro2.radio = false;
+        carro2.motor = "2.0 Turbo";
+        carro2.ano = 2016;
+
+        carro1.compararAno(carro2);
     }
 }
--- src/Carro.java
+++ src/Carro.java
@@ -2,4 +2,18 @@ public class Carro {
     int bancos;
     boolean radio;
     String motor;
+    int ano;
+
+    public Carro compararAno(Carro outroCarro) {
+        if (this.ano > outroCarro.ano) {
+            System.out.println("Carro mais novo: ano " + this.ano + ", motor " + this.motor);
+            return this;
+        } else if (outroCarro.ano > this.ano) {
+            System.out.println("Carro mais novo: ano " + outroCarro.ano + ", motor " + outroCarro.motor);
+            return outroCarro;
+        } else {
+            System.out.println("Ambos os carros são do mesmo ano.");
+            return this;
+        }
+    }
 }
