ab50fbaf36bced15546e6489faafc8830cd7066c
Kauli Robinson Kleemann <kaulirk@outlook.com>
2026-08-23 18:48:30 -0300

    Adicionado: carro1 e motorista1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        Carro carro1 = new Carro();
+        Motorista motorista1 = new Motorista("Carlos Nêmesis", carro1);
+      
+        carro1.bancos = 4;
+        carro1.radio = true;
+        carro1.motor = "1.0 Flex";
+
+        motorista1.exibirMotoristaECarro();
     }
 }
