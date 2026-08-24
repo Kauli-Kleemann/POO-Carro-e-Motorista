12922a274dcbe8504303758194731c2cb2f178bd
Kauli  Kleemann <0431746@feevale.br>
2026-08-24 08:57:38 -0300

    Adicionado: classe Motor, com os atributos potencia e tipo. Adicionado: método exibirMotor da classe Motor.

---
+++ src/Motor.java
@@ -0,0 +1,10 @@
+public class Motor {
+    int potencia;
+    String tipo;
+
+    public void exibirMotor() {
+        System.out.println("\nDetalhes do motor:");
+        System.out.println("Potência: " + potencia + ";");
+        System.out.println("Tipo: " + tipo + ".");
+    }
+}
