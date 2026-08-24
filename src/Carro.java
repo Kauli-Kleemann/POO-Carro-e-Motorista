720b33f0a9e7528906e07a06b3e1c04e1645ba35
Kauli  Kleemann <0431746@feevale.br>
2026-08-24 11:32:43 -0300

    Adicionado: método exibirDetalhes na classe Carro. Removido: this.motor no método compararAno da classe Carro.

--- src/Carro.java
+++ src/Carro.java
@@ -13,14 +13,23 @@ public class Carro {
 
     public Carro compararAno(Carro outroCarro) {
         if (this.ano > outroCarro.ano) {
-            System.out.println("\nCarro mais novo: ano " + this.ano + ", motor " + this.motor);
+            System.out.println("\nCarro mais novo: ano " + this.ano + ".");
             return this;
         } else if (outroCarro.ano > this.ano) {
-            System.out.println("\nCarro mais novo: ano " + outroCarro.ano + ", motor " + outroCarro.motor);
+            System.out.println("\nCarro mais novo: ano " + outroCarro.ano + ".");
             return outroCarro;
         } else {
             System.out.println("\nAmbos os carros são do mesmo ano.");
             return this;
         }
+
+        }
+
+        public void exibirDetalhes() {
+            System.out.println("\nDetalhes do carro:");
+            System.out.println("Ano: " + this.ano + ";");
+            System.out.println("Bancos: " + this.bancos + ";");
+            System.out.println("Rádio: " + this.radio + ";");
+            this.motor.exibirMotor();
     }
 }
